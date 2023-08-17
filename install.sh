0fc38324a8fe355b113def81b3639185f9e3d40e
Jacob Hummer <jcbhmr@outlook.com>
2023-08-17 15:23:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 set -ex
 
-cat <<EOF
-----------------------------------------------
+# https://github.com/devcontainers/features/blob/9fd5daf06b40794a067c4cdbb8e258d99eeefc24/src/node/install.sh
+apt_get_update() {
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
 
-               YOU ARE AWESOME!
+# https://github.com/devcontainers/features/blob/9fd5daf06b40794a067c4cdbb8e258d99eeefc24/src/node/install.sh
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
 
-----------------------------------------------
-EOF
+check_packages curl ca-certificates
+
+curl -sSf https://install.surrealdb.com | sh
